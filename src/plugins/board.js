808d2afd541d739535ce442edb48e3b15fd3a979
proceane <proceane1@gmail.com>
2021-06-06 15:40:18 +0900

    feat : 상세데이터 조회 코드 추가

--- src/plugins/board.js
+++ src/plugins/board.js
@@ -1,7 +1,12 @@
 export default {
   getList (fb, kdcd) {
     fb.database().ref('data/' + kdcd).on('value', (sn) => {
-        console.log(sn)
+      console.log(sn.val())
+    })
+  },
+  getDetail (fb, kdcd, ctcd, asno) {
+    fb.database().ref('data/' + kdcd + '/' + ctcd + '/' + asno).on('value', (sn) => {
+      console.log(sn.val())
     })
   }
 }
